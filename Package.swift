eec4fe3c7de6f14282d86b777e57a07b04af1eb5
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-12-06 21:06:03 -0600

    error

--- Package.swift
+++ Package.swift
@@ -30,31 +30,3 @@ let package = Package(
         dependencies: ["SwiftMatrix"]) 
    ]
 )
-
-
-let package = Package(
-    name: "MetalView",
-	platforms: [
-		.macOS(.v11),
-		.iOS(.v13)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MetalView",
-            targets: ["MetalView"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MetalView",
-            dependencies: []),
-        .testTarget(
-            name: "MetalViewTests",
-            dependencies: ["MetalView"])
-    ]
-)
