2ed4dd0681bd92c4e58042855187a00db4232cbd
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-12-06 21:08:22 -0600

    error

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftMatrix",
+    name: "SwiftMatrixLibrary",
 	platforms: [
 		.macOS(.v11),
 		.iOS(.v13)],
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library( 
             name: "SwiftMatrix", 
-            targets: ["SwiftMatrix"]),
+            targets: ["SwiftMatrixTarget"]),
       ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target( 
-            name: "SwiftMatrix", 
+            name: "SwiftMatrixTarget", 
             dependencies: []),
        .testTarget(
-        name: "SwiftMatrixTests", 
-        dependencies: ["SwiftMatrix"]) 
+            name: "SwiftMatrixTests", 
+            dependencies: ["SwiftMatrixTarget"]),
    ]
 )
