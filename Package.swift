7a072eb6f86e1af75c27841b2a921b3dc318c728
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-12-06 20:50:26 -0600

    update trying to find

--- Package.swift
+++ Package.swift
@@ -5,10 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftMatrix",
-	
+	platforms: [
+		.macOS(.v11),
+		.iOS(.v13)],
+   
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library( name: "SwiftMatrix", targets: ["Matrix"]),
+        .library( name: "SwiftMatrix", targets: ["SwiftMatrix"]),
       ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,7 +20,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target( name: "Matrix", dependencies: []),
-        
+        .target( name: "SwiftMatrix", dependencies: []),
+       .testTarget(name: "SwiftMatrix", dependencies["SwiftMatrix"]) 
    ]
 )
---
+++ Tests/SwiftMatrix/SwiftMatrix.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftMatrix
+
+final class SwiftMatrix: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+       //XCTAssertEqual(MetalView().text, "Hello, World!")
+    }
+}
